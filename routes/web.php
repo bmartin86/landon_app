e743c4dbf58f062aa4cfa11d8d1633864f519430
bmartin86 <iMuy11pMpk>
2021-09-22 16:20:52 +0200

    neuspješna ruta

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ContentsController;
+use App\Http\Controllers\ClientController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,5 +16,26 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    //return view('welcome');
+    return '<h3>Landon App Page</h3>';
 });
+
+Route::get('/About', function () {
+    $response_arr = [];
+    $response_arr['author'] = 'BP';
+    $response_arr['version'] = '0.1.1';
+    return $response_arr;
+    //return '<h3>About</h3>';
+});
+
+
+Route::get('/home', function () {
+    $data = [];
+    $data ['version'] = '0.1.1';
+    return view('welcome', $data);
+});
+
+
+Route::get('/di', [ClientController::class, 'di']);
+
+Route::get('/ex', [ContentsController::class, 'home']);
