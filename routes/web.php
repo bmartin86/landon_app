2a56e432597438307adfd5066fec84c49c3333e7
bmartin86 <iMuy11pMpk>
2021-09-22 16:51:58 +0200

    popunjavanje controllera

--- routes/web.php
+++ routes/web.php
@@ -38,4 +38,15 @@ Route::get('/home', function () {
 
 Route::get('/di', [ClientController::class, 'di']);
 
+Route::get('/clients', [ClientController::class, 'index']);
+Route::get('/clients/new', [ClientController::class, 'newClient']);
+Route::post('/clients/new', [ClientController::class, 'create']);
+Route::get('/clients/{client_id}', [ClientController::class, 'show']);
+Route::post('/clients/{client_id}', [ClientController::class, 'modify']);
+
+Route::get('/reservations/{client_id}', [RoomsController::class, 'checkAvailableRooms']);
+Route::post('/reservations/{client_id}', [RoomsController::class, 'checkAvailableRooms']);
+
+Route::get('/book/room/{client_id}/{room_id}/{date_in}/{date_out}', [ReservationController::class, 'bookRoom']);
+
 Route::get('/ex', [ContentsController::class, 'home']);
