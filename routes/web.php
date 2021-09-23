04e5807effe635b3c9214b81789045e4e466eb77
bmartin86 <iMuy11pMpk>
2021-09-23 14:54:03 +0200

    popunjavanje view-ova

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContentsController;
 use App\Http\Controllers\ClientController;
+use App\Http\Controllers\RoomsController;
 
 /*
 |--------------------------------------------------------------------------
