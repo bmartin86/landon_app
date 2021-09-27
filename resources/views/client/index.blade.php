53ba96fe63d126c52ab7321fbfafde0e55fa2ed9
bmartin86 <iMuy11pMpk>
2021-09-27 19:42:44 +0200

    dodana metoda Client modify

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -23,7 +23,7 @@
                 <td>{{ $client->title }}. {{ $client->name }} {{ $client->last_name }} </td>
                 <td>{{ $client->email }}</td>
                 <td>
-                  <a class="hollow button" href="{{ route('show_client', ['client_id'=> $client->id]) }}">EDIT</a>
+                  <a class="hollow button warning" href="{{ route('show_client', ['client_id'=> $client->id]) }}">EDIT</a>
                   <a class="hollow button warning" href="{{ route('check_room', ['client_id'=> $client->id]) }}">BOOK A ROOM</a>
                 </td>
               </tr>
